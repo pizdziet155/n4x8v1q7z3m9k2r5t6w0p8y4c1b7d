b28fbc598f989a108ea50f574c156b56ac3e407b
pizdziet155 <knijah@wp.pl>
2026-06-23 20:05:55 +0200

    Refactor sendTo function for URL redirection

--- assets/bar.js
+++ assets/bar.js
@@ -2,7 +2,7 @@
 var params = new URLSearchParams(window.location.search);
 
 function sendTo(url){
-    location.href = "./" + url + ".html" + params;
+    location.href = `${url}?` + params;
 }
 
 document.querySelectorAll(".bottom_element_grid").forEach((element) => {
